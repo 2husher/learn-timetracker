78cdebdca4ac93a1f9a72c006e8ef71fc4cd2176
Alex Izotov <martinandrudy@gmail.com>
2014-12-15 21:17:11 +0300

    added scopes for work and project
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,9 @@
 class Work < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  scope :fullday, -> { where('hours >= 8') }
+  scope :recent, -> { where('datetimeperformed > ?', Time.now - 7.days) }
+  #scope :recent__7, -> { where('datetimeperformed > "#{Time.now - 7.days}"') } DOESN'T WORK
+  scope :recent_7, -> { where("datetimeperformed > '#{Time.now - 7.days}'") }
 end
