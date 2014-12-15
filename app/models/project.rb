78ed5d9584610423106b432717ba7a77ac444dda
Alex Izotov <martinandrudy@gmail.com>
2014-12-15 23:40:41 +0300

    fixed validation
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,7 @@ class Project < ActiveRecord::Base
   has_many :works
   has_many :users, through: :works
 
-  validates :name, presence: true,
-                   length: { minimum: 5 }
+  validates :name, length: { minimum: 5 }
   validates :company, presence: true
   validates :default_rate, numericality: { only_integer: true, greater_than_or_equal: 50 }
 
