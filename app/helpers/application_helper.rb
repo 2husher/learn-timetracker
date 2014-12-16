ca158498cb2e06ea4827d8d426ab8cb626e0d265
Alex Izotov <martinandrudy@gmail.com>
2014-12-17 02:09:17 +0300

    added display_nav_item and nav_item helper method
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  def nav_items
+    [
+       {
+        controllername: 'companies',
+        displaytext: 'Companies',
+        linkurl: companies_path
+       },
+       {
+        controllername: 'projects',
+        displaytext: 'Projects',
+        linkurl: projects_path
+       },
+       {
+        controllername: 'works',
+        displaytext: 'Works',
+        linkurl: works_path
+       }
+    ]
+  end
+
+  def display_nav_item(controllername, displaytext, linkurl)
+     raw("<li #{controller_name == controllername ? "class='active'" : ''}> #{link_to displaytext, linkurl}</li>")
+  end
 end
