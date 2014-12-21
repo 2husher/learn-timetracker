d601b0691776cf502cbdc943b4509f5f607bf97d
Alex Izotov <martinandrudy@gmail.com>
2014-12-21 17:43:57 +0300

    refactored sidebar
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,15 @@ module ApplicationHelper
        }
     ]
   end
+
+  def sidebar
+    unless ['sessions', 'registrations'].include?(controller_name)
+      config = {}
+      config[:index_title] = "All #{controller_name.capitalize}"
+      config[:index_path]  = "#{controller_name}_path"
+      config[:new_title]   = "New #{controller_name.singularize.capitalize}"
+      config[:new_path]    = "new_#{controller_name.singularize}_path"
+      render partial: 'shared/sidebar', locals: { config: config }
+    end
+  end
 end
